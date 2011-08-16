257fe6dcc01290577c875db8681d40582659853b
chersanya <chersanya@gmail.com>
2011-08-16 16:45:01 +0400

    Initial commit
    --HG--
    extra : convert_revision : acafd15248ad8ad76b5869864bfc857f19f550b6

---
+++ OAuthProtocolException.cs
@@ -0,0 +1,25 @@
+﻿namespace OAuthCSharp
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    public class OAuthProtocolException : Exception
+    {
+        public OAuthProtocolException()
+            : base()
+        { }
+
+        public OAuthProtocolException(string message)
+            : base(message)
+        { }
+
+        public OAuthProtocolException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        protected OAuthProtocolException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
+    }
+}
---
+++ Tokens.cs
@@ -0,0 +1,53 @@
+﻿namespace OAuthCSharp
+{
+    using System;
+    using System.Collections.Specialized;
+    using System.Web;
+
+    public class TokenBase
+    {
+        public string Token { get; protected set; }
+        public string TokenSecret { get; protected set; }
+        public NameValueCollection AdditionalProperties { get; private set; }
+
+        public TokenBase()
+        {
+            AdditionalProperties = new NameValueCollection();
+        }
+
+        public void Init(string tokenResponse)
+        {
+            if (tokenResponse == null) { throw new ArgumentNullException("tokenResponse"); }
+
+            var parts = tokenResponse.Split('&');
+            foreach (var part in parts)
+            {
+                var nameValue = part.Split('=');
+                if (nameValue.Length == 2)
+                {
+                    switch (nameValue[0])
+                    {
+                        case "oauth_token":
+                            Token = HttpUtility.UrlDecode(nameValue[1]);
+                            break;
+                        case "oauth_token_secret":
+                            TokenSecret = HttpUtility.UrlDecode(nameValue[1]);
+                            break;
+                        default:
+                            AdditionalProperties.Add(nameValue[0], HttpUtility.UrlDecode(nameValue[1]));
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    public class RequestToken : TokenBase
+    {
+        public bool CallbackConfirmed { get; set; }
+    }
+
+    public class AccessToken : TokenBase
+    {
+    }
+}
